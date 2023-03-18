b25d84ef8051a3f21c04bd61526fc32f03baed5b
karank85 <kkjas85@gmail.com>
2023-03-18 15:58:16 +0700

    implemented the bot successfully

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import openapi
 import discord
-import os
+import openai
 
-client = discord.Client()
+bot = discord.Client()
 
-client.run(open("TOKEN.txt").read())
+openai.api_key = open("API_KEY").read().strip("\n")
+
+
+@bot.event
+async def on_ready():
+    print("ChatGPT discord bot is ready!")
+
+
+@bot.event
+async def on_message(message):
+    prompt = message.content
+    if message.author == bot.user:
+        return
+    if prompt.startswith("!ask"):
+        question = prompt.split("!ask", 1)[1]
+        output = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question},
+            ],
+        )
+        await message.channel.send(output["choices"][0]["message"]["content"])
+
+
+bot.run(open("TOKEN.txt").read())
