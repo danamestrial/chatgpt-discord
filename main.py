5b018f62ded5a8d31cd4495ab2b90f1b3b612cf8
karank85 <kkjas85@gmail.com>
2023-03-18 15:59:54 +0700

    Revert "implemented the bot successfully"
    This reverts commit b25d84ef8051a3f21c04bd61526fc32f03baed5b.

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
+import openapi
 import discord
-import openai
+import os
 
-bot = discord.Client()
+client = discord.Client()
 
-openai.api_key = open("API_KEY").read().strip("\n")
-
-
-@bot.event
-async def on_ready():
-    print("ChatGPT discord bot is ready!")
-
-
-@bot.event
-async def on_message(message):
-    prompt = message.content
-    if message.author == bot.user:
-        return
-    if prompt.startswith("!ask"):
-        question = prompt.split("!ask", 1)[1]
-        output = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": question},
-            ],
-        )
-        await message.channel.send(output["choices"][0]["message"]["content"])
-
-
-bot.run(open("TOKEN.txt").read())
+client.run(open("TOKEN.txt").read())
